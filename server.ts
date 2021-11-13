b7d74eaee96af3a0d7e4217ab2d0edb31eae6f5e
eduardo <=>
2021-11-13 10:12:46 -0300

    feat: index

--- server.ts
+++ server.ts
@@ -1,11 +1,26 @@
-import express from 'express'
 import 'dotenv/config'; 
+import cors from 'cors';
+import express from 'express';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
 
 const { PORT, DB_URL } = process.env;
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { 
     res.send('HELLO')
 })
+
+//conexao com mongodb
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(DB_URL || '', { useNewUrlParser: true });
+
+// Middlewares
+app.use(morgan('dev')); // log das requisições 
+app.use(express.json()); //Used to parse JSON bodies
+app.use(express.urlencoded()); //Parse URL-encoded bodies
+app.use(cors());
+
 
 app.listen('4000');
